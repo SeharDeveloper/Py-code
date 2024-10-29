fb533b804722e08925a90759172f4ad122305d0b
Haleema Sehar <Haleemasehr@gmail.com>
2024-10-29 12:06:21 +0500

    updated .py

--- __pycache__/loops.py/01_p.py
+++ __pycache__/loops.py/01_p.py
@@ -1,7 +1,51 @@
-#Write a program to count +ve numbers from the given list
-number=[1,-2,3,-4,5,-6,7,-8,9,-10]
-positive_number_count=0
-for num in number:
-    if num>0:
-        positive_number_count += 1
-print("Final count of postive numbers is: ", positive_number_count)
+#for loop using list--->basic programming practise
+#Number List Printer: [1, 2, 3, 4, 5]
+my_list=[1, 2, 3, 4, 5]
+for num in range(6):
+    print(num)
+#[1, 2, 3, 4, 5]loop use kar ke is list ke numbers ka total nikaalna hai.
+my_items=[1,2,3,4,5]
+total=0
+for num in my_items:
+   total+=num#total=num+1
+print("Total is: ", total)
+#[1, 2, 3, 4, 5, 6]. Tumne sirf even numbers ko print karna hai
+list1=[1,2,3,4,5,6]
+for i in list1:
+    if i %2==0:
+        print(i)
+#["apple", "banana", "apple", "orange"]count karna hai ke total kitni items hain.  
+list2=["apple", "banana", "apple", "orange"]
+count =0
+for item in list2:
+    count+=1
+print("Total items in list:", count)
+#["Ali", "Sara", "Ahmed"]har name ke sath "Hello" print karna hai, jaise "Hello Ali".
+list2=["Ali", "Sara", "Ahmed"]
+for i in list2:
+    print("Hello ", i)
+#[1, 2, 3, 4, 5]number ka square nikaal ke print karna hai.
+list4=[1,2,3,4,5]
+square=0
+for i in list4:
+    square=i**2
+    print("Square is ",square)
+#loop use karke is list ko reverse order mein print karna hai.
+list3=["Ali", "Ahmad", "Sara"]
+for i in range(len(list3) -1,-1,-1):
+    print(list3 [i])
+# loop use karke numbers 1 se 10 tak print karne hain.
+num=0
+for i in range(1,11):
+    print(i)
+#Find Maximum in List.
+list1=[3, 5, 2, 8, 6]
+max_number=list1[0]
+for number in list1:
+    if number>max_number:
+        max_number=number
+print("maximum number is ", max_number)
+#Alphabet Printer
+import string
+alphabet_list=list(string.ascii_lowercase)
+print(alphabet_list)
