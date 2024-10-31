79d60aa05565c918f385a17fa30d910553226600
Haleema Sehar <Haleemasehr@gmail.com>
2024-10-31 21:31:49 +0500

    words.py

--- 01_basics/Words.py
+++ 01_basics/Words.py
@@ -3,6 +3,8 @@
 #Counts the number of words in the sentence.
 #Prints the results
 sentence=str(input("Enter a sentence here: "))
-for i in sentence:
-   sentence.split()
-   i.len(sentence)
+words=sentence.split()
+length=len(sentence)
+print(f"{sentence} has {length} letters")
+
+    
