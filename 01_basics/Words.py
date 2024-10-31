aac8f4634e2463c6677d0b8d7853537cec210646
Haleema Sehar <Haleemasehr@gmail.com>
2024-10-31 20:41:15 +0500

    Path changed

---
+++ 01_basics/Words.py
@@ -0,0 +1,8 @@
+#Task: Count Words in a Sentence
+#Takes a sentence as input from the user.
+#Counts the number of words in the sentence.
+#Prints the results
+sentence=str(input("Enter a sentence here: "))
+for i in sentence:
+   sentence.split()
+   i.len(sentence)
